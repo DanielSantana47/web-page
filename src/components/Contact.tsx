df1f5fc93a4d0487475b6741ae925df097e9cc3e
Daniel Santana <danielsantanachastel@gmail.com>
2023-08-10 16:54:00 -0300

    reveal effect

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./Button"
+import { Reveal } from "./Reveal"
 
 export const Contact = ()=> {
     return(
@@ -6,15 +7,23 @@ export const Contact = ()=> {
             <div className="container mx-auto w-full h-full">
                 <div className="w-32 h-32 bg-zinc-800 top-0 right-1/2 -mr-14 -mt-16 rounded-full absolute "></div>
                 <div className="flex flex-col items-center">
-                    <h2 className="text-3xl mt-16 font-semibold mb-12 md:mb-24">GET IN TOUCH</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 px-4">
-                        <input type="text" className=" py-2 px-4 flex-1 text-zinc-200 bg-zinc-500 rounded-md outline-none placeholder:text-zinc-200/50" placeholder="NAME"/>
-                        <input type="text"  className=" py-2 px-4 flex-1 text-zinc-200 bg-zinc-500 rounded-md outline-none placeholder:text-zinc-200/50" placeholder="EMAIL"/>
-                        <textarea name="" id="" className="resize-none py-2 px-4 text-zinc-200 bg-zinc-500 rounded-md outline-none placeholder:text-zinc-200/50 md:col-span-2" placeholder="MESSAGE"></textarea>
-                    </div>
-                    <div className="mb-24">
-                        <Button text="SUBMIT"/>
-                    </div>
+                    <Reveal>
+                        <h2 className="text-3xl mt-16 font-semibold mb-12 md:mb-24">GET IN TOUCH</h2>
+                    </Reveal>
+                    <Reveal>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 px-4">
+                            
+                                <input type="text" className=" py-2 px-4 flex-1 text-zinc-200 bg-zinc-500 rounded-md outline-none placeholder:text-zinc-200/50" placeholder="NAME"/>
+                            
+                                <input type="text"  className=" py-2 px-4 flex-1 text-zinc-200 bg-zinc-500 rounded-md outline-none placeholder:text-zinc-200/50" placeholder="EMAIL"/>
+                                <textarea name="" id="" className="resize-none py-2 px-4 text-zinc-200 bg-zinc-500 rounded-md outline-none placeholder:text-zinc-200/50 md:col-span-2" placeholder="MESSAGE"></textarea>
+                        </div>
+                    </Reveal>
+                    <Reveal>
+                        <div className="mb-24">
+                            <Button text="SUBMIT"/>
+                        </div>
+                    </Reveal>
 
                 </div>
             </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,24 @@
 import { Button } from "./Button"
 import { MobileMenu } from "./MobileMenu"
 import { PcMenu } from "./PcMenu"
+import { Reveal } from "./Reveal"
 
 export const Home = ()=> {
     return(
-        <header id="Home" className=" text-white bg-no-repeat bg-center bg-cover bg-[url(https://cdn.pixabay.com/photo/2016/03/26/13/09/workspace-1280538_1280.jpg)]">
+        <header id="Home" className=" text-white bg-no-repeat bg-center bg-cover bg-[url(https://cdn.pixabay.com/photo/2016/03/26/13/09/workspace-1280538_1280.jpg)]">    
             <PcMenu/>
             <MobileMenu/>
             <div className="w-full h-full flex flex-col justify-center bg-gradient-to-b from-transparent from-30% to-zinc-800 ">
+            <Reveal>
                 <div className="py-48 w-full xl:max-w-4xl px-8  md:px-32 xl:24 flex flex-col items-center">
-                    <h1 className="text-center text-6xl md:text-9xl font-bold mb-6">I AM DESIGNER</h1>
-                    <h2 className="text-2xl md:text-3xl font-serif text-center font-semibold mb-8 md:mb-12"> <i>I Make Wonderful Things</i></h2>
-                    <p className="md:text-lg text-justify text-md">Lorem ipsum dolor sit amet. Sit saepe reiciendis et enim architecto sed vero reiciendis ea nostrum culpa qui sint alias. Aut dolores soluta ad harum quis ex dolor repellat hic possimus iste sed tempore tempore cum fugit natus. Ut internos error sit quisquam doloribus aut ducimus tenetur. Sit ipsum minus rem dolores omnis est dolorem sunt est quod perspiciatis.</p>
+                        <>
+                            <h1 className="text-center text-6xl md:text-9xl font-bold mb-6">I AM DESIGNER</h1>
+                            <h2 className="text-2xl md:text-3xl font-serif text-center font-semibold mb-8 md:mb-12"> <i>I Make Wonderful Things</i></h2>
+                            <p className="md:text-lg text-justify text-md">Lorem ipsum dolor sit amet. Sit saepe reiciendis et enim architecto sed vero reiciendis ea nostrum culpa qui sint alias. Aut dolores soluta ad harum quis ex dolor repellat hic possimus iste sed tempore tempore cum fugit natus. Ut internos error sit quisquam doloribus aut ducimus tenetur. Sit ipsum minus rem dolores omnis est dolorem sunt est quod perspiciatis.</p>
+                        </>
                     <Button text="READ MORE"/>
                 </div>
+                    </Reveal>
             </div>
         </header>
     )
